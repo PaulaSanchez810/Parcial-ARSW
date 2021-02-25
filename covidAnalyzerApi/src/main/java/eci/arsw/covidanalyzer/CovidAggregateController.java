a3d40fcc74ee40b08e39ad9182560ad373627a10
Paula Andrea Guevara Sanchez <paula.guevara-s@mail.escuelaing.edu.co>
2021-02-25 08:59:15 -0500

    metodo GET

--- covidAnalyzerApi/src/main/java/eci/arsw/covidanalyzer/CovidAggregateController.java
+++ covidAnalyzerApi/src/main/java/eci/arsw/covidanalyzer/CovidAggregateController.java
@@ -3,16 +3,12 @@ package eci.arsw.covidanalyzer;
 import eci.arsw.covidanalyzer.model.Result;
 import eci.arsw.covidanalyzer.model.ResultType;
 import eci.arsw.covidanalyzer.service.ICovidAggregateService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
 
 @RestController
 public class CovidAggregateController {
@@ -35,11 +31,52 @@ public class CovidAggregateController {
 
     //TODO: Implemente todos los metodos GET que hacen falta.
 
-    @RequestMapping(value = "/covid/result/true-positive", method = RequestMethod.GET)
-    public ResponseEntity<?> getTruePositiveResult() throws Exception {
-        covidAggregateService.getResult(ResultType.TRUE_POSITIVE);
-        return ResponseEntity.ok("Hello World");
+    @RequestMapping(value = "/covid/result/true-positive", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getTruePositiveResult() {
+        try {
+            return new ResponseEntity.ok(covidAggregateService.getResult(ResultType.TRUE_POSITIVE));
+            covidAggregateService.getResult(ResultType.FALSE_POSITIVE);
+            covidAggregateService.getResult(ResultType.FALSE_NEGATIVE);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity("Los datos porsivos son:", HttpStatus.NOT_FOUND);
+        }
     }
+    @RequestMapping(value = "/covid/result/true-negative", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?>getTrueNegativeResult() {
+        try {
+            return new ResponseEntity.ok(covidAggregateService.getResult(ResultType.TRUE_NEGATIVE));
+            covidAggregateService.getResult(ResultType.FALSE_POSITIVE);
+            covidAggregateService.getResult(ResultType.FALSE_NEGATIVE);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity("Los datos porsivos son:", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(value = "/covid/result/false-negative", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?>getFalseNegativeResult() {
+        try {
+            return new ResponseEntity.ok(covidAggregateService.getResult(ResultType.FALSE_NEGATIVE));
+            covidAggregateService.getResult(ResultType.FALSE_POSITIVE);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity("Los datos porsivos son:", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(value = "/covid/result/false-positive", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?>getFalsePositiveveResult() {
+        try {
+            return new ResponseEntity.ok(covidAggregateService.getResult(ResultType.FALSE_POSITIVE));
+            covidAggregateService.getResult(ResultType.FALSE_POSITIVE);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity("Los datos porsivos son:", HttpStatus.NOT_FOUND);
+        }
+    }
+
+
 
 
     //TODO: Implemente el método.
---
+++ covidAnalyzerApi/src/main/java/eci/arsw/covidanalyzer/T.java
@@ -0,0 +1,4 @@
+package eci.arsw.covidanalyzer;
+
+public class T {
+}
